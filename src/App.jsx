da840adbbcc478f1fe88a1fa3db6587047f25196
윤수영 <yunssu8428@gamail.com>
2025-12-04 11:14:08 +0900

    1단계

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MovieCard from "./components/MovieCard";
+import movieList from "./data/movieListData.json";
+import './index.scss'
+import logo from "./assets/logo.png"; 
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Grid } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+
+export default function App() {
+  const [movies] = useState(movieList.results || []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className='title-wrap'>
+      <h1 className='movie_list' style={{ color: "white" }}>영화 목록</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <section className="movie-grid">
+        <Swiper
+         modules={[Navigation, Pagination, Grid]} // 사용할 스위퍼 모듈
+         grid={{ rows: 1, fill: "row" }}          // 1행 그리드
+         spaceBetween={16}                        // 카드간간격
+         slidesPerView={4}                        // 화면에 4장만 표시
+         navigation                               // 포스터 좌우로 넘기는 기능
+         pagination={{ clickable: true }}         //
+         style={{ paddingBottom: "20px" }}
+       >
+
+      
+      
+        {movies.map((movie)=> (
+          <SwiperSlide key={movie.id}>
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            posterPath={movie.poster_path}
+            title={movie.title}
+            rating={movie.vote_average}
+            tagline={movie.tagline || "" }
+            overview={movie.overview}
+            genres={movie.genres}
+            runtime={movie.runtime}
+            release_date={movie.release_date}
+          />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      </section>
+    </main>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import MovieDetail from "./components/MovieDetail.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route element={<Layout />} >
+        <Route path="/" element={<App />} />
+        <Route path="/details" element={<MovieDetail />}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
