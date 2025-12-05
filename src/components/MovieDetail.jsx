d4606f26aec1f351062e5863bee6a7010090171f
윤수영 <yunssu8428@gamail.com>
2025-12-05 15:23:18 +0900

    미션2

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,41 +1,67 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
-import { IMAGE_BASE } from "../constants"; 
-import  MovieDetailData  from "../data/movieDetailData.json" 
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IMAGE_BASE, TMDB_BASE_URL, TMDB_API_KEY } from "../constants"; 
+
+const options = { method: "GET", headers: { accept: "application/json" } };
 
 export default function MovieDetail() {        
-  const location = useLocation();           
-  const { movie } = location.state || {};    //카드에서 넘겨준 객체를 가져옴
-  const detailMovie = MovieDetailData;       //Detail JSON을 detail movie로 사용
-  if (!movie) {                          
-    return <p>영화 정보를 불러올 수 없습니다.</p>;
-  }
+  const { id } = useParams();           
+  const [movie, setMovie] = useState(null);    //카드에서 넘겨준 객체를 가져옴
+  const [loading, setLoading] = useState(true);
+  const [error, setError]  = useState("");
+  
+  useEffect(() =>{
+    async function fetchDetail(){
+      try {
+        setLoading(true);
+        const res = await fetch(
+          `${TMDB_BASE_URL}/movie/${id}?api_key=${TMDB_API_KEY}&language=ko-KR`,options
+        );
+        if (!res.ok) throw new Error ("상세 호출 실패");  // 404처럼 호출 실패하는 경우에 앱이 멈추지 않도록함 
+        const data = await res.json();
+        setMovie(data);
+      }catch(err){
+        console.error(err);
+        setError("영화 정보를 불러올 수 없습니다.");
+      }finally{
+        setLoading(false);
+      }
+    }
 
-  return (
-    <div className="detail-wrapper">
-      <div
-        className="backdrop"               //클릭 시 배경사진영역
-        style={{
-          backgroundImage: `url(${IMAGE_BASE}${movie.backdrop_path || movie.posterPath})`,
-        }}  // 백드롭 주소가 있으면 사용하고 없으면 포스터이미지 사용
+    if(id) fetchDetail();
+  },[id]);
+
+    if (loading) return <p>로딩 중...</p>;
+    if (error) return <p>{error}</p>;
+    if (!movie) return <p>영화 정보를 불러올 수 없습니다.</p>;
+  
+  //Detail JSON을 detail movie로 사용
+
+      return (
+         <div className="detail-wrapper">
+           <div className="backdrop"
+             style={{
+              backgroundImage: `url(${IMAGE_BASE + (movie.backdrop_path || movie.poster_path)})`,
+        }}
       />
       <section className="detail-content">
         <img
           className="detail-poster"    // 상세 페이지 왼쪽 포스터
-          src={`${IMAGE_BASE}${movie.posterPath}`}
+          src={`${IMAGE_BASE}${movie.poster_path}`}
           alt={movie.title}
         />
         <div className="info">          
           <h2>{movie.title}</h2>  
           <p className="tagline">{movie.tagline}</p>
           <p className="rating">
-            평점: {movie.rating?.toFixed(1)}({detailMovie.vote_count}명)
+            평점: {movie.vote_average?.toFixed(1)}({movie.vote_count}명)
           </p>
-          <p className="genres">{movie.genres?.map(g => g.name).join(" · ")}</p>
+          <p className="genres">
+            {movie.genres?.map(g => g.name).join("·")}</p>
           <h3>줄거리</h3>
           <p>{movie.overview}</p>
           <p>
-            런타임: {detailMovie.runtime}분 <br/> 
+            런타임: {movie.runtime}분 <br/> 
             개봉일: {movie.release_date} <br />
           </p>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
     <Routes>
       <Route element={<Layout />} >
         <Route path="/" element={<App />} />
-        <Route path="/details" element={<MovieDetail />}/>
+        <Route path="/details/:id" element={<MovieDetail />}/>
       </Route>
     </Routes>
   </BrowserRouter>
