4d83c9384f529535be73a09fbdcfb061cdd51c24
윤수영 <yunssu8428@gamail.com>
2025-12-05 18:34:53 +0900

    2단계완료

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const TMDB_ACCESS_TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
 export const TMDB_API_KEY = import.meta.env.VITE_TMDB_ACCESS_KEY;
 
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-export const IMAGE_BASE = "https://image.tmdb.org/t/p/w500";s
+export const IMAGE_BASE = "https://image.tmdb.org/t/p/w500";
