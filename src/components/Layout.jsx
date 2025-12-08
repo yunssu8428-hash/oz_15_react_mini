4c99127155f8e44b874e8ae4f77aec88a83981e3
윤수영 <yunssu8428@gamail.com>
2025-12-08 10:48:15 +0900

    3단계완료

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 
-export default function Layout(){
+export default function Layout() {
+  const [searchQuery, setSearchQuery] = useState("");
+  // 검색어를 여기서 받아서 NavBar에 값을 넘겨주고
+  
+
   return (
     <>
-      <NavBar />
-      <Outlet />
+      <NavBar
+        searchQuery={searchQuery}
+        onChangeSearchQuery={setSearchQuery}
+      />
+      <Outlet context={{ searchQuery }} />
     </>
   );
 }
+// App에 contextAPI를 써서 값을 내려줬음
+// searchQuery는 현재 검색어 값
+// onChangeSearchQuery는 검색어를 바꾸는 함수로 사용
